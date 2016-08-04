b4f719b390b7e8e8893706670579b2cb9425ffe4
Magnus <magnus.wennerberg@gmail.com>
2016-08-04 14:36:55 +0200

    Commiting creation of tables.

---
+++ create_table_one.sql
@@ -0,0 +1,5 @@
+--Creating table one
+
+create table one (id   number(12),
+                  type varchar2(10),
+		  data varchar2(4000));
---
+++ create_table_two.sql
@@ -0,0 +1,5 @@
+Creating table two
+
+create table two (id   number(12),
+                  type varchar2(10),
+		  desc varchar2(100));
